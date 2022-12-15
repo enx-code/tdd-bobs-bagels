dc9ba2ff0d23ee4daa11c172c3734ebee1322ab1
Enky <enkh6at@yahoo.com>
2022-12-15 09:40:04 +0000

    add

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,14 +1,14 @@
 const { Basket } = require('../src/basket')
 
-describe("Basket", ()=>{
-    let basket = new Basket()
-       
-
-    })
-    // it('should create a new basket', () => {
-    //   expect(basket).toBeInstanceOf(Basket)
-    //   expect(basket.basketQuantity).toBe(5)
-    //   expect(basket.items.length).toBe(0)
-    // })
-
-    
+describe('Basket', () => {
+  let basket
+  beforeEach(() => {
+    basket = new Basket()
+  })
+  it("nvnv", (()=>{}))
+})
+// it('should create a new basket', () => {
+//   expect(basket).toBeInstanceOf(Basket)
+//   expect(basket.basketQuantity).toBe(5)
+//   expect(basket.items.length).toBe(0)
+// })
